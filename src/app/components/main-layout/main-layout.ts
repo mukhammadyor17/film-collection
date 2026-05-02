4b608b70f612093b5d00ac0f4b33e0a9d2702621
dior1726 <muxammad_909@mail.ru>
2026-05-02 14:36:10 +0500

    feat: generate base components and pages

---
+++ src/app/components/main-layout/main-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PageHeader } from '../page-header/page-header';
+import { PageFooter } from '../page-footer/page-footer';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, PageHeader, PageFooter],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css',
+})
+export class MainLayout {}
