bd70729f14df4021dcaff1f2c24b712b25189bf4
dior1726 <muxammad_909@mail.ru>
2026-05-02 14:34:30 +0500

    init: start film-collection app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { AboutPage } from './pages/about-page/about-page';
+import { FavoritePage } from './pages/favorite-page/favorite-page';
+import { MainLayout } from './components/main-layout/main-layout';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        title: 'FC | Home Page',
+        component: HomePage,
+      },
+      {
+        path: 'about',
+        title: 'FC | About Page',
+        component: AboutPage,
+      },
+      {
+        path: 'favotites',
+        title: 'FC | Favorite Page',
+        component: FavoritePage,
+      },
+    ],
+  },
+];
